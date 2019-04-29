b9567392b448716eae44b211ef0b3eadbaf1cb34
Chia-Man Hung <chiaman@robots.ox.ac.uk>
2019-04-29 15:36:17 +0100

    Add actions and messages for vision.

--- orion_actions/setup.py
+++ orion_actions/setup.py
@@ -5,7 +5,6 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
    #  don't do this unless you want a globally visible script
-   # scripts=['bin/myscript'], 
    packages=['orion_actions'],
    package_dir={'': 'src'}
 )
